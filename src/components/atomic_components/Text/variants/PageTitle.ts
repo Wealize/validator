2398450d532b96ae6b8afcf5a3174e51464ab21f
Arnau Zapata Delgado <azapata@Arnaus-MacBook-Pro.local>
2023-01-20 13:31:58 +0100

    update text

--- src/components/atomic_components/Text/variants/PageTitle.ts
+++ src/components/atomic_components/Text/variants/PageTitle.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { H1Styled } from "..";
 
-const PageTitle = styled(H1Styled).attrs({ title: true })`
+const PageTitle = styled(H1Styled)`
     font-family: Montserrat;
+    font-weight: bold;
 `;
 
 export default PageTitle;
--- src/components/atomic_components/Text/variants/Title.ts
+++ src/components/atomic_components/Text/variants/Title.ts
@@ -4,8 +4,9 @@ import styled from "styled-components";
 import { H2Styled } from "..";
 
 
-const Title = styled(H2Styled).attrs({ title: true })`
+const Title = styled(H2Styled)`
   font-family: Montserrat;
+  font-weight: bold;
 `;
 
 export default Title;
