e133c8c71a1a1a1f256c46b63fc1a3d427296ede
Arnau Zapata Delgado <azapata@Arnaus-MacBook-Pro.local>
2023-07-13 11:03:25 +0200

    eslint

--- src/components/atomic_components/Text/variants/DescriptionText.ts
+++ src/components/atomic_components/Text/variants/DescriptionText.ts
@@ -1,9 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import { pStyled } from "..";
+import styled from 'styled-components'
+
+import { pStyled } from '..'
 
 const DescriptionText = styled(pStyled)`
-    font-family: "Muli", sans-serif;
-`;
+  font-family: 'Muli', sans-serif;
+`
 
-export default DescriptionText;
+export default DescriptionText
--- src/components/atomic_components/Text/variants/Title.ts
+++ src/components/atomic_components/Text/variants/Title.ts
@@ -1,11 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-import { H2Styled } from "..";
+import styled from 'styled-components'
 
+import { H2Styled } from '..'
 
 const Title = styled(H2Styled)`
   font-family: Montserrat;
   font-weight: bold;
-`;
+`
 
-export default Title;
+export default Title
